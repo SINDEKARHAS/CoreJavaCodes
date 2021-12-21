20a77a9f16251112ed8c8d16e4f2c77674c0f1f8
MehraRuby <96409460+MehraRuby@users.noreply.github.com>
2021-12-21 12:58:28 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class Hello
+{
+	public static void main(String... g)
+	{
+		System.out.println("Hello");
+	}
+}
+
+class World
+{
+	public static void main(String... g)
+	{
+		System.out.println("World");
+	}
+	
+}
